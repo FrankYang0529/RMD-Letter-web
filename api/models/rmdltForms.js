75e34370eec2e4f64ed2c1b7d969529b04f1e9a7
winone520 <j70915@gmail.com>
2017-04-14 15:12:55 +0800

    form models add sub document->option

--- api/models/rmdltForms.js
+++ api/models/rmdltForms.js
@@ -2,13 +2,17 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
+const Options = new Schema({
+  option: String
+});
+
 const Letter = new Schema({
   projID: String,
   title: String,
   questions: [{
       questionTitle: String,
       questionType: String,
-      options: [{ type: String }],
+      options: [Options],
       require: Boolean
     }]
 });
