babdef57d16937c0cc1ca7fc16f94229695a1894
Kuan-Wen Liu <j70915@gmail.com>
2017-08-14 21:24:21 +0800

    modify project models amd api

--- api/models/projects.js
+++ api/models/projects.js
@@ -2,11 +2,27 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
+const HbrBody = new Schema({
+  title: String, // ex: [公告]推薦信截止時間
+  text: String,
+  img: String,
+  file: String,
+  timestamp: Date,
+});
+
+const Hbr = new Schema({
+  email: String,
+  phone: String,
+  startTime: Date,
+  endTime: Date,
+  body: [HbrBody],
+});
+
 const Projects = new Schema({
   ownerID: String,
   titleZh: String,
-  hbr: String,
-  subdomainName: { type: String, required: true,unique: true },
+  hbr: Hbr,
+  subdomainName: { type: String, required: true, unique: true },
 });
 
 module.exports = mongoose.model('projs', Projects);
