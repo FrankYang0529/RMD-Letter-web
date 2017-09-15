a65695e17ec84d029e4998d88982cebb0b822027
Kuan-Wen Liu <j70915@gmail.com>
2017-09-16 01:31:27 +0800

    finish student api and frontend connected, comment textSet used part

--- routes/rmdPeople.js
+++ routes/rmdPeople.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const routes = require('../api/controllers/rmdPeople');
 
 //  projects management
-router.get('/:projID/:stuID', routes.fillInPage);
-router.post('/:projID/:stuID', routes.fillIn);
+router.get('/:projID/:rmdPersonID/:stuID', routes.fillInPage);
+router.post('/:projID/:rmdPersonID/:stuID', routes.fillIn);
 
 module.exports = router;
